1d5e6ec288a90b733b7646d5c8c1a4b94820f127
vandanasolapure <127217079+vandanasolapure@users.noreply.github.com>
2025-06-03 10:16:18 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+let score ="33aaa"
+
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+console.log("-----------xyz--------")
+let xyz=null;
+console.log(xyz)
+console.log(typeof xyz)
+let valueInNumber1=Number(xyz)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+let valueInBoolean= Boolean(xyz)
+console.log(typeof valueInBoolean)
+console.log(valueInBoolean)
+
+console.log("-----------undef--------")
+let undef=undefined;
+console.log(undef)
+console.log(typeof undef)
+let valueInNumber2=Number(undef)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+let valueInBoolean1= Boolean(undef)
+console.log(typeof valueInBoolean1)
+console.log(valueInBoolean1)
+
+
+console.log("-----Boolean--------")
+let isLoggedIn=0;
+console.log(isLoggedIn);
+
+
+let valueInBoolean3=Boolean(isLoggedIn)
+console.log(typeof valueInBoolean3)
+console.log(valueInBoolean3)
+
+
+console.log("-----Boolean New--------")
+let isLoggedIn1="";
+console.log(isLoggedIn);
+
+
+let valueInBoolean13=Boolean(isLoggedIn1)
+console.log(typeof valueInBoolean13)
+console.log(valueInBoolean13)
+
+
+
+console.log("--------String-------")
+
+let num=33
+console.log(num)
+
+let stringNum=String(num)
+console.log(typeof stringNum)
+console.log(stringNum)
