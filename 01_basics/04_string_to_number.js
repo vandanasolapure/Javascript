284c193f0e55d1470532d7fe1f69fbee5c9c9d5a
vandanasolapure <127217079+vandanasolapure@users.noreply.github.com>
2025-06-12 05:43:02 +0000

    Strings

---
+++ 01_basics/04_string_to_number.js
@@ -0,0 +1,19 @@
+let value=3
+let negvalue=-3
+console.log(negvalue)
+
+
+let str1="Hello"
+let str2=" Vandana"
+let str3=str1+str2
+console.log(str3)
+
+
+
+console.log("-------New--------")
+
+console.log("1" +2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1+2+"2")
---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,38 @@
+const name="Vandana"
+const repoCount=10
+// basic way which is not used not a days
+// console.log(name+repoCount+" Value")
+
+
+//mordern way 
+/*
+console.log(`Hello guys my name is ${name} and my repocount is ${repoCount}`)
+
+
+
+//other way to declare string in js
+
+const getName= new String('Vandana')
+console.log(getName.length)
+console.log(getName.charAt('3'))
+console.log(getName.indexOf('S'))
+
+const newString = getName.substring(0,4)
+console.log(newString)
+
+const newString1 = getName.substring(-8,4)
+console.log(newString1)
+
+const newString2 = getName.slice(0,4)
+console.log(newString2)
+
+
+
+const newString3 = getName.slice(-6,4)
+console.log(newString3)
+
+*/
+
+
+let splitstring="V-S-S"
+console.log(splitstring.split('-'))
