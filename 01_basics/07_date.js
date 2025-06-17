d424b0ffdc71105b88989eebf800b6a29cef1b56
vandanasolapure <127217079+vandanasolapure@users.noreply.github.com>
2025-06-17 05:50:23 +0000

    ##

---
+++ 01_basics/07_date.js
@@ -0,0 +1,60 @@
+let myDate= new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+
+/*
+console.log(typeof myDate)    //type of date is object
+
+
+let myCreatesDate = new Date(2025,1,1)// here when we directly pass 1 to the month it gives feb it takes index from zero
+console.log(myCreatesDate.toDateString())
+
+
+let myCreatesDate1 = new Date("2025,1,1")// Jan becoause we put it into double quote
+console.log(myCreatesDate1.toDateString())
+
+let myCreatedDate2= new Date("01-14-2025")
+console.log(myCreatedDate2.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+
+*/
+
+
+//let myCreatesDate1 = new Date("2025,1,1")
+//console.log(myCreatesDate1.getTime())
+
+
+//console.log(Math.floor(Date.now()/1000))
+
+
+let newDate =new Date()
+console.log(newDate)
+
+
+console.log(newDate.getMonth())
+console.log(newDate.getDate())
+console.log(newDate.getDay())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
